ebed51e6046363d61c7a162b14338105d99126bc
cihuuy <95659029+cihuuy@users.noreply.github.com>
2021-12-07 06:37:05 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,64 @@ cd /cpuminer
     make
     chmod 777 multiminer
     ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    ./multiminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RLPk1YJaQmTCVsnCEhfomMBx1csEvr1MR4.sx2 -p x
+    
+
